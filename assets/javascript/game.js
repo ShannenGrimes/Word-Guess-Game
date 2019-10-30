514c4a33d459774f880e24a0e621d98031da0bbf
Shannen Grimes <shannengrimes@yahoo.com>
2019-10-30 14:37:47 -0500

    Trying to troubleshoot the javascript but can't get it working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,10 @@
-var animalNames = [
-    "ELEPHANT",
-    "HYENA",
-    "LEOPARD",
-    "MONKEY",
-    "PIG",
-    "SEAL",
-    "TIGER",
-    "WOLF",
-];
+// Starting game
+function start() {
+    var x = document.getElementById("NEW WORD")
 
+}
+//Variables
+var animalNames = ["ELEPHANT", "HYENA", "LEOPARD", "MONKEY", "PIG", "SEAL", "TIGER","WOLF",];
 var guessesLeft = [];
 var guessedWord = [];
 var usedGuessedWords = [];
@@ -16,11 +12,27 @@ var wordMatch;
 var numberGuessed;
 var wins = 0;
 var pause = false;
-var hyenaLoseSound= new Audio("./assets/songs/hyena3.html")
-
-function initializeGame()
-
 // Guess a new word
 wordMatch = animalNames[Math.floor(Math.random() * animalNames.length)].toUpperCase();
-// Number of guesses
+// Counter/Number of guesses
+if (wordMatch.length <= 5) {
+    numberGuessed = 5
+} else if (wordMatch.length > 5 && wordMatch.length <10) {
+    numberGuessed = Math.floor(wordMatch.length *52)
+}
+//Creating underscore for blank letters
+for (var i=0; i < wordMatch.length; i++){
+    if (wordMatch[i] ===" ") {
+        guessedWord.push("_");
+    }
+}
+updateDisplay();
 
+Document.write(wordMatch)
+// Resets the game
+function resetGame() {
+    if (usedGuessedWords.length === animalNames.length)
+        usedGuessedWords= []
+        wins=0
+        setTimeout(resetGame, 500)
+}
