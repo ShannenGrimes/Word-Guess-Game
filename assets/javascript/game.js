caceba8266edde9cb9a5c6f2a1048e77337b0fef
Shannen Grimes <shannengrimes@yahoo.com>
2019-10-29 19:01:35 -0500

    Trying newjavascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,18 @@ var animalNames = [
     "WOLF",
 ];
 
-@media (min-width: 576px)
-.jumbotron {
-    padding: 2rem 2rem;
-}
+var guessesLeft = [];
+var guessedWord = [];
+var usedGuessedWords = [];
+var wordMatch;
+var numberGuessed;
+var wins = 0;
+var pause = false;
+var hyenaLoseSound= new Audio("./assets/songs/hyena3.html")
+
+function initializeGame()
+
+// Guess a new word
+wordMatch = animalNames[Math.floor(Math.random() * animalNames.length)].toUpperCase();
+// Number of guesses
+
